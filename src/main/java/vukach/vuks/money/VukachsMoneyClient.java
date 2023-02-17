8750c7b89fb8ad5bc6fb43657da0036ca79107b0
Vukach <106126881+Vukach@users.noreply.github.com>
2023-02-16 19:46:58 -0800

    Add files via upload

---
+++ src/main/java/vukach/vuks/money/VukachsMoneyClient.java
@@ -0,0 +1,14 @@
+package vukach.vuks.money;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class VukachsMoneyClient implements ClientModInitializer
+{
+
+    @Override
+    public void onInitializeClient() {
+        
+        
+    }
+	
+}
